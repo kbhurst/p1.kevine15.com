7e618cfb8510ef8f3bc31aa592a753cc0d5f88c9
Kevin Hurst <kevin.b.hurst@gmail.com>
2013-09-16 19:32:16 -0600

    Cleanup of code

--- index.php
+++ index.php
@@ -1,29 +1,25 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE html 
-     PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-<!--project 1 Kevin Hurst E15 created on 09/15/2013-->
-
-   <link rel="stylesheet" type="text/css" href="style.css" />
-
+  <meta charset="utf-8">
+  <!--project 1 Kevin Hurst E15 created on 09/15/2013-->
+   
    <title>P1 Kevin Hurst</title>
- </head>
+   
+     <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
 
-
- 	<body>
-		<div id="outline">
-			<div id="inside">
+    <body>
+	  <div id="outline">  <!--define outer boundry-->
+	  <div id="inside">
+		
+     	  <div class="projectname">project one</div>  <!--centering element-->
 			
-			<div class="projectname">project one</div>
-			
-      <div id="homelink"><a href="home.html">code editor</a></div>
-      <div id="aboutmelink"><a href="about.html">about me</a></div>
-     <!-- <div id="validatelink"><a href="validate.html">validate link working</a></div>-->
-			</div>
-		</div>
+         <div id="homelink"><a href="codeeditor.html">code editor</a></div>  <!--links-->
+         <div id="aboutmelink"><a href="about.html">about me</a></div>
+ 	   </div>
+	   </div>
 		
 	</body>
 
